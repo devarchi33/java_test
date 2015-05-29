eaa027cadae8a3eacbb90276df26a6bddbb2f83e
skyfly33 <imfly7@naver.com>
2015-05-29 14:27:39 +0900

    @visit count package JedisHelper 추가.

--- src/test/jedis/visitcount/VisitCount.java
+++ src/test/jedis/visitcount/VisitCount.java
@@ -1,5 +1,26 @@
 package test.jedis.visitcount;
 
+import redis.clients.jedis.Jedis;
+
 public class VisitCount {
 
+	private JedisHelper jedisHelper;
+	private Jedis jedis;
+	private final String KEY_EVENT_CLICK_TOTAL = "event:click:total";
+	private final String KEY_EVENT_CLICK = "event:click:";
+
+	/**
+	 * 방문 횟수 처리를 위한 클래스 생성자
+	 * 
+	 * @param jedisHelper
+	 */
+	public VisitCount(JedisHelper jedisHelper) {
+		this.jedisHelper = jedisHelper;
+		this.jedis = jedisHelper.getConnection();
+	}
+
+	public Long addVisit(String eventId) {
+		this.jedis.incr(KEY_EVENT_CLICK_TOTAL);
+		return this.jedis.incr(KEY_EVENT_CLICK + eventId);
+	}
 }
