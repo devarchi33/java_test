1b5be0c4000d24f677db7c04019db7cad8dc2a5c
skyfly33 <imfly7@naver.com>
2015-06-02 10:46:47 +0900

    @Cart 예제 class 완성.

--- src/skyfly33/jedis/cart/Cart.java
+++ src/skyfly33/jedis/cart/Cart.java
@@ -1,5 +1,7 @@
 package skyfly33.jedis.cart;
 
+import java.util.Set;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -10,24 +12,24 @@ import redis.clients.jedis.Jedis;
 public class Cart {
 
 	private Jedis jedis;
-	private static JSONObject cartInfo;
+	private JSONObject cartInfo;
 	private String userNo;
 	private static final String KEY_CART_LIST = ":cart:product";
 	private static final String KEY_CART_PRODUCT = ":cart:productid:";
 	private static final String JSON_PRODUCT_LIST = "products";
-	private static final int EXPIRE = 60;
-	
-	public Cart(JedisHelper helper, String userNo){
+	private static final int EXPIRE = 60 * 60 * 24 * 3;
+
+	public Cart(JedisHelper helper, String userNo) {
 		this.jedis = helper.getConnection();
 		this.userNo = userNo;
 		this.cartInfo = getCartInfo();
 	}
-	
-	private JSONObject getCartInfo() {
-		String productInfo = this.jedis.get(this.userNo+KEY_CART_LIST);
-		if(null == productInfo || "".equals(productInfo))
+
+	public JSONObject getCartInfo() {
+		String productInfo = this.jedis.get(this.userNo + KEY_CART_LIST);
+		if (productInfo == null || "".equals(productInfo))
 			return makeEmptyCart();
-		
+
 		try {
 			JSONParser parser = new JSONParser();
 			return (JSONObject) parser.parse(productInfo);
@@ -36,59 +38,85 @@ public class Cart {
 			return makeEmptyCart();
 		}
 	}
-	
-	private JSONObject makeEmptyCart() {
+
+	public JSONObject makeEmptyCart() {
 		JSONObject cart = new JSONObject();
 		cart.put(JSON_PRODUCT_LIST, new JSONArray());
 		return cart;
 	}
-	
+
 	public int flushCart() {
 		JSONArray products = (JSONArray) this.cartInfo.get(JSON_PRODUCT_LIST);
 		for (int i = 0; i < products.size(); i++) {
-			this.jedis.del(this.userNo+KEY_CART_PRODUCT+products.get(i));
+			this.jedis.del(this.userNo + KEY_CART_PRODUCT + products.get(i));
 		}
-		
+
 		this.jedis.set(this.userNo + KEY_CART_LIST, "");
 		return products.size();
 	}
-	
-	public String addProduct(String productNo, String productName, int quantity){
+
+	public String addProduct(String productNo, String productName, int quantity) {
 		JSONArray products = (JSONArray) this.cartInfo.get(JSON_PRODUCT_LIST);
-		
 		products.add(productNo);
-		
-		this.jedis.set(this.userNo + KEY_CART_LIST, this.cartInfo.toJSONString());
-		
+
+		this.jedis.set(this.userNo + KEY_CART_LIST,
+				this.cartInfo.toJSONString());
+
 		JSONObject product = new JSONObject();
 		product.put("productNo", productNo);
 		product.put("productName", productName);
-		product.put("quantity",quantity);
-		
-		String productKey = this.userNo + KEY_CART_PRODUCT + productNo;
-		return this.jedis.setex(productKey, EXPIRE, product.toJSONString());
+		product.put("quantity", quantity);
+
+		String poductKey = this.userNo + KEY_CART_PRODUCT + productNo;
+		return this.jedis.setex(poductKey, EXPIRE, product.toJSONString());
 	}
-	
-	
-	static JedisHelper helper = JedisHelper.getInstance();
-	public static void main(String[] args){
-		Cart cart = new Cart(helper,"user1");
-		System.out.println("before : "+cartInfo);
-		cart.addProduct("pNo1", "pName1", 1);
-		System.out.println("after : "+cartInfo);
-		
-		JSONObject tempObject = new JSONObject();
-		JSONArray tempArray = new JSONArray();
-		tempObject.put("arr", tempArray);
-		
-		System.out.println(tempObject);
-		JSONArray testArray = (JSONArray) tempObject.get("arr");
-		System.out.println(tempObject);
-		testArray.add("tempData");
-		System.out.println(tempObject);
-		
-		
-		
-		
+
+	public int deleteProduct(String[] productNo) {
+		JSONArray products = (JSONArray) this.cartInfo.get(JSON_PRODUCT_LIST);
+		int result = 0;
+
+		for (String item : productNo) {
+			products.remove(item);
+			result += this.jedis.del(this.userNo + KEY_CART_PRODUCT + item);
+		}
+
+		this.jedis
+				.set(this.userNo, KEY_CART_LIST, this.cartInfo.toJSONString());
+		return result;
+	}
+
+	public JSONArray getProductList() {
+		boolean isChanged = false;
+		JSONArray products = (JSONArray) this.cartInfo.get(JSON_PRODUCT_LIST);
+		JSONArray result = new JSONArray();
+		String value = null;
+
+		for (int i = 0; i < products.size(); i++) {
+			value = this.jedis.get(this.userNo + KEY_CART_PRODUCT
+					+ products.get(i));
+
+			if (value == null)
+				isChanged = true;
+			else
+				result.add(value);
+		}
+
+		if (isChanged) {
+			this.jedis.set(this.userNo + KEY_CART_LIST,
+					this.cartInfo.toJSONString());
+		}
+
+		return result;
+	}
+
+	public int flushCartDeprecate() {
+		Set<String> keys = this.jedis
+				.keys(this.userNo + KEY_CART_PRODUCT + "*");
+		for (String key : keys) {
+			this.jedis.del(key);
+		}
+
+		this.jedis.set(this.userNo + KEY_CART_LIST, "");
+		return keys.size();
 	}
 }
