4f9578b58b4b1b9b8ae04fa1a02947146f2ddda9
skyfly33 <imfly7@naver.com>
2015-06-03 15:02:06 +0900

    @jedis PostLike 처리 예제 추가.

---
+++ src/skyfly33/jedis/likeposting/LikePostingTest.java
@@ -0,0 +1,102 @@
+package skyfly33.jedis.likeposting;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LikePostingTest {
+
+	static JedisHelper helper;
+	private LikePosting likePosting;
+	private static Random rand = new Random();
+	private static final int POSTING_COUNT = 20;
+	private static final int TESTUSER = rand.nextInt(10000000);
+	private static String[] POSTLIST = new String[POSTING_COUNT];
+
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		helper = JedisHelper.getInstance();
+		for (int i = 0; i < POSTLIST.length; i++) {
+			POSTLIST[i] = String.valueOf(i + 1);
+		}
+	}
+
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		helper.destroyPool();
+	}
+
+	
+	@Before
+	public void setUp() throws Exception {
+		this.likePosting = new LikePosting(helper);
+		assertNotNull(this.likePosting);
+	}
+
+	
+	@Test
+	public void testLike() {
+		String postingNo = String.valueOf(this.rand.nextInt(POSTING_COUNT));
+		if (this.likePosting.isLiked(postingNo, String.valueOf(TESTUSER))) {
+			this.likePosting.unLike(postingNo, String.valueOf(TESTUSER));
+		}
+		assertTrue(this.likePosting.like(postingNo, String.valueOf(TESTUSER)));
+	}
+
+	
+	@Test
+	public void testUnLike() {
+		String postingNo = String.valueOf(this.rand.nextInt(POSTING_COUNT));
+		if(this.likePosting.isLiked(postingNo, String.valueOf(TESTUSER))){
+			assertTrue(this.likePosting.unLike(postingNo, String.valueOf(TESTUSER)));
+		}else{
+			assertTrue(this.likePosting.like(postingNo, String.valueOf(TESTUSER)));
+			assertTrue(this.likePosting.unLike(postingNo, String.valueOf(TESTUSER)));
+		}
+	}
+	
+	
+	@Test
+	public void testGetLikecount() {
+		String postingNo = String.valueOf(this.rand.nextInt(POSTING_COUNT));
+		if(this.likePosting.isLiked(postingNo, String.valueOf(TESTUSER))) {
+			assertTrue(this.likePosting.unLike(postingNo, String.valueOf(TESTUSER)));
+		}
+		
+		Long prevCount = this.likePosting.getLikeCount(postingNo);
+		this.likePosting.like(postingNo, String.valueOf(TESTUSER));
+		assertEquals(this.likePosting.getLikeCount(postingNo), new Long(prevCount + 1));
+	}
+	
+	
+	@Test
+	public void testIsLiked() {
+		String postingNo = String.valueOf(this.rand.nextInt(POSTING_COUNT));
+		this.likePosting.like(postingNo, String.valueOf(TESTUSER));
+		assertTrue(this.likePosting.isLiked(postingNo, String.valueOf(TESTUSER)));
+	}
+	
+	@Test
+	public void testDeleteLikeInfo() {
+		String postingNo = "A1234567890";
+		this.likePosting.like(postingNo, String.valueOf(postingNo));
+	}
+	
+	
+	@Test
+	public void testRandomLike() {
+		for (int i = 0; i < POSTING_COUNT; i++) {
+			String sudoRandomUser = String.valueOf(rand.nextInt(10000000));
+			this.likePosting.like(String.valueOf(i), sudoRandomUser);
+		}
+	}
+}
---
+++ src/skyfly33/jedis/likeposting/test.java
@@ -0,0 +1,12 @@
+package skyfly33.jedis.likeposting;
+
+import java.util.Random;
+
+public class test {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int a = rand.nextInt(100);
+		System.out.println("rand : " + a);
+	}
+}
